26c0fe6546a6577b26167f511f7b3c64a795984f
hlgtdv <158814832+hlgtdv@users.noreply.github.com>
2024-02-18 22:14:54 +0100

    Writing the Yaml index

--- src/remote-data-downloader/local/DataDownloader.groovy
+++ src/remote-data-downloader/local/DataDownloader.groovy
@@ -2,6 +2,8 @@ package local
 
 import java.util.logging.*
 
+import org.yaml.snakeyaml.Yaml
+
 class DataDownloader {
 
 	class Ref {
@@ -29,25 +31,34 @@ class DataDownloader {
 	def workDir
 	def outputDir
 	def mapItemToItems
+	def yaml
 
 	DataDownloader(config) {
 		this.config = config
 		this.workDir = new File(this.config.workDirectory)
 		this.outputDir = new File(this.config.outputDirectory)
 		this.mapItemToItems = [ : ]
-		
-		this.init()
+		this.yaml = new Yaml()
 	}
 	
-	def init() {
+	def begin(itemRef) {
 		this.workDir.mkdirs()
 		this.outputDir.mkdirs()
 		this.workDir.eachFileRecurse { it.delete() }
 		this.outputDir.eachFileRecurse { it.delete() }
+		
+		this.mapItemToItems['root'] = itemRef.toString()
 	}
 
-	def fetch(itemType, itemId, isFetchSubItems = false) {
+	def fetch(itemType, itemId, level=0) {
+		if (! (itemType in this.config.allowedItemTypes)) {
+			return
+		}
 		def itemRef = new Ref(itemType, itemId)
+
+		if (level == 0) {
+			this.begin(itemRef)
+		}
 
 		LOGGER.log(Level.INFO, "Downloading: ${itemRef} ...")
 		
@@ -56,10 +67,13 @@ class DataDownloader {
 		
 		def subItemsRefs = this.findSubItemsRefsIn(rootItem)
 		
-		for (subItemRef in subItemsRefs) {				
-			this.fetch(subItemRef.type, subItemRef.id, itemType in this.config.allowedItemTypes)
+		for (subItemRef in subItemsRefs) {
+			this.fetch(subItemRef.type, subItemRef.id, level + 1)
 			
-			this.mapItemToItems.computeIfAbsent(itemRef, k -> []) << subItemRef			
+			this.mapItemToItems.computeIfAbsent(itemRef.toString(), k -> []) << subItemRef.toString()			
+		}
+		if (level == 0) {
+			this.end()
 		}
 	}
 
@@ -76,5 +90,9 @@ class DataDownloader {
 			}
 
 		return subItemsRefs
+	}
+
+	def end() {
+		new File("${this.workDir}/index.yaml") << this.yaml.dump(mapItemToItems)
 	}
 }
