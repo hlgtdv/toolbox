a8777b217375cb8a50b41f4de77bc0092e0bd989
hlgtdv <158814832+hlgtdv@users.noreply.github.com>
2025-03-27 19:14:48 +0100

    Component no more depends on spring-webmvc

--- WSExecutionFilter.java
+++ WSExecutionFilter.java
@@ -8,15 +8,20 @@ import javax.servlet.FilterChain;
 
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import javax.servlet.ServletException;
 
+import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -25,8 +30,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.util.StreamUtils;
-import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.apache.commons.io.output.TeeOutputStream;
 
 @WebFilter("/*")
 public class WSExecutionFilter implements Filter {
@@ -38,42 +42,41 @@ public class WSExecutionFilter implements Filter {
     )
         throws ServletException, IOException
     {
-        CachedHttpServletRequest cachedHttpServletRequest =
-            new CachedHttpServletRequest((HttpServletRequest) servletRequest);
-        ContentCachingResponseWrapper contentCachingResponseWrapper = 
-            new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+        CacheServletRequestWrapper cacheServletRequestWrapper =
+            new CacheServletRequestWrapper((HttpServletRequest) servletRequest);
+        CacheServletResponseWrapper cacheServletResponseWrapper = 
+            new CacheServletResponseWrapper((HttpServletResponse) servletResponse);
 
-        String httpMethod = cachedHttpServletRequest.getMethod();
-        String requestUrl = cachedHttpServletRequest.getRequestURL().toString();
-        String queryString = cachedHttpServletRequest.getQueryString();
-        String requestBody = cachedHttpServletRequest.getCachedBody();
+        String httpMethod = cacheServletRequestWrapper.getMethod();
+        String requestUrl = cacheServletRequestWrapper.getRequestURL().toString();
+        String queryString = cacheServletRequestWrapper.getQueryString();
+        String requestBody = cacheServletRequestWrapper.payload;
 
         queryString = queryString == null ? "" : queryString;
 
         System.out.println("### REQUEST BODY BEGIN ###########################");
         System.out.println(httpMethod + " " + requestUrl + queryString);
 
-        for (String headerName : Collections.list(cachedHttpServletRequest.getHeaderNames())) {
-            String headerValue = cachedHttpServletRequest.getHeader(headerName);
+        for (String headerName : Collections.list(cacheServletRequestWrapper.getHeaderNames())) {
+            String headerValue = cacheServletRequestWrapper.getHeader(headerName);
 
             System.out.println(headerName + ": " + headerValue);
         }
         System.out.println(requestBody);
         System.out.println("### REQUEST BODY END   ###########################");
 
-        filterChain.doFilter(cachedHttpServletRequest, contentCachingResponseWrapper);
+        filterChain.doFilter(cacheServletRequestWrapper, cacheServletResponseWrapper);
 
-        int httpStatus = contentCachingResponseWrapper.getStatus();
-        String responseBody = new String(contentCachingResponseWrapper.getContentAsByteArray(),
-            contentCachingResponseWrapper.getCharacterEncoding());
+        int httpStatus = cacheServletResponseWrapper.getStatus();
+        String responseBody = cacheServletResponseWrapper.getContent();
 
-        contentCachingResponseWrapper.copyBodyToResponse();
+//        cacheServletResponseWrapper.copyBodyToResponse();
 
         System.out.println("### RESPONSE BODY BEGIN ###########################");
         System.out.println(httpStatus);
 
-        for (String headerName : contentCachingResponseWrapper.getHeaderNames()) {
-            String headerValue = contentCachingResponseWrapper.getHeader(headerName);
+        for (String headerName : cacheServletResponseWrapper.getHeaderNames()) {
+            String headerValue = cacheServletResponseWrapper.getHeader(headerName);
 
             System.out.println(headerName + ": " + headerValue);
         }
@@ -81,62 +84,146 @@ public class WSExecutionFilter implements Filter {
         System.out.println("### RESPONSE BODY END   ###########################");
     }
 
-    private class CachedHttpServletRequest extends HttpServletRequestWrapper {
+    class CacheServletRequestWrapper extends HttpServletRequestWrapper {
 
-        private byte[] cachedBody;
+        public final String payload;
 
-        public CachedHttpServletRequest(HttpServletRequest request) throws IOException {
+        public CacheServletRequestWrapper(HttpServletRequest request) throws ServletException {
             super(request);
-            InputStream requestInputStream = request.getInputStream();
-            this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+
+            // read the original payload into the payload variable
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            try {
+                // read the payload into the StringBuilder
+                InputStream inputStream = request.getInputStream();
+                if (inputStream != null) {
+                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    char[] charBuffer = new char[128];
+                    int bytesRead = -1;
+                    while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                        stringBuilder.append(charBuffer, 0, bytesRead);
+                    }
+                } else {
+                    // make an empty string since there is no payload
+                    stringBuilder.append("");
+                }
+            } catch (IOException ex) {
+                throw new ServletException("Error reading the request payload", ex) { };
+            } finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException iox) {
+                        // ignore
+                    }
+                }
+            }
+            payload = stringBuilder.toString();
         }
 
         @Override
         public ServletInputStream getInputStream() throws IOException {
-            return new CachedBodyServletInputStream(this.cachedBody);
-        }
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(payload.getBytes());
+            ServletInputStream inputStream = new ServletInputStream() {
+                public int read() throws IOException {
+                    return byteArrayInputStream.read();
+                }
 
-        @Override
-        public BufferedReader getReader() throws IOException {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
-            return new BufferedReader(new InputStreamReader(byteArrayInputStream));
-        }
+                @Override
+                public void setReadListener(javax.servlet.ReadListener readListener) {
+                    // NOOP
+                } 
 
-        public String getCachedBody()
-            throws UnsupportedEncodingException
-        {
-            return new String(this.cachedBody, this.getCharacterEncoding());
+                @Override
+                public boolean isReady() {
+                    return true;
+                }
+
+                @Override
+                public boolean isFinished() {
+                    return byteArrayInputStream.available() == 0;
+                }
+            };
+            return inputStream;
         }
     }
 
-    private class CachedBodyServletInputStream extends ServletInputStream {
+    class CacheServletResponseWrapper extends HttpServletResponseWrapper {
 
-        private InputStream cachedBodyInputStream;
+        TeeServletOutputStream teeStream;
 
-        public CachedBodyServletInputStream(byte[] cachedBody) {
-            this.cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
+        PrintWriter teeWriter;
+
+        ByteArrayOutputStream bos;
+
+        public CacheServletResponseWrapper(HttpServletResponse response) {
+            super(response);
         }
 
-        public void setReadListener(javax.servlet.ReadListener readListener) {
-            // NOOP
-        } 
-
-        @Override
-        public boolean isReady() {
-            return true;
-        }
-
-        @Override
-        public int read() throws IOException {
-            return cachedBodyInputStream.read();
+        public String getContent() throws IOException {
+            return bos.toString();
         }
 
         @Override
-        public boolean isFinished() {
-            try {
-                return cachedBodyInputStream.available() == 0;
+        public PrintWriter getWriter() throws IOException {
+
+            if (this.teeWriter == null) {
+                this.teeWriter = new PrintWriter(new OutputStreamWriter(getOutputStream()));
             }
-            catch (IOException e) {
+            return this.teeWriter;
+        }
+
+        @Override
+        public ServletOutputStream getOutputStream() throws IOException {
+
+            if (teeStream == null) {
+                bos = new ByteArrayOutputStream();
+                teeStream = new TeeServletOutputStream(getResponse().getOutputStream(), bos);
+            }
+            return teeStream;
+        }
+
+        @Override
+        public void flushBuffer() throws IOException {
+            if (teeStream != null) {
+                teeStream.flush();
+            }
+            if (this.teeWriter != null) {
+                this.teeWriter.flush();
+            }
+        }
+
+        public class TeeServletOutputStream extends ServletOutputStream {
+
+            private final TeeOutputStream targetStream;
+
+            public TeeServletOutputStream(OutputStream one, OutputStream two) {
+                targetStream = new TeeOutputStream(one, two);
+            }
+
+            @Override
+            public void write(int b) throws IOException {
+                this.targetStream.write(b);
+            }
+
+            public void flush() throws IOException {
+                super.flush();
+                this.targetStream.flush();
+            }
+
+            public void close() throws IOException {
+                super.close();
+                this.targetStream.close();
+            }
+
+            @Override
+            public void setWriteListener(javax.servlet.WriteListener writeListener) {
+                // NOOP
+            } 
+
+            @Override
+            public boolean isReady() {
                 return true;
             }
         }
